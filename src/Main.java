5a682113e11490dbb64b90fee2d125fc488f3481
Mausam Shrestha <mashrestha@salud.unm.edu>
2021-03-01 06:06:41 +0000

    Upload New File

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+/**
+ * Main class for the domino game, which starts the game!
+ * @author: Mausam Shrestha
+ * @date: 02/18/2020
+ * @project: 2 - Domino game
+ * @version: 1
+ * @UNMId: 101865530
+ * @course: CS351
+ */
+package CS351;
+
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+    }
+}
